e552ce3ce5b5a42fad61855ecee4f1346b170044
hiroki-seishima <yuguiqingdao@gmail.com>
2025-12-16 19:32:06 +0900

    課題の提出です。

---
+++ trainingjava/src/com/example/main6/Main6_4.java
@@ -0,0 +1,11 @@
+package com.example.main6;
+
+public class Main6_4 {
+    public static void main(String[] args) {
+        int a =10;
+        int b =2;
+        int total = Main6_2.tasu(a,b);
+        int delta = Main6_2.hiku(a,b);
+        System.out.println("足すと" + total + "、引くと" + delta);
+    }
+}

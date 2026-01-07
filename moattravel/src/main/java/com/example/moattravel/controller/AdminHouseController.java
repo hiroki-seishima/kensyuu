fbc34b6f1869b6800db18ee4f0687255be497d4c
hiroki-seishima <yuguiqingdao@gmail.com>
2026-01-07 18:47:43 +0900

    本日の学習

---
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -0,0 +1,36 @@
+package com.example.moattravel.controller;
+
+// import java.util.List;  ページ毎表示にするために削除(8.3)
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.moattravel.entity.House;
+import com.example.moattravel.repository.HouseRepository;
+
+@Controller
+@RequestMapping("/admin/houses")
+public class AdminHouseController {
+    private final  HouseRepository houseRepository;
+
+    public AdminHouseController(HouseRepository houseRepository) {
+        this.houseRepository  = houseRepository;
+    }
+
+    @GetMapping
+    public String index(Model model,@PageableDefault(page  = 0, size = 10,sort = "id", direction = Direction.ASC) Pageable pageable){
+        //  List<House> houses = houseRepository.findAll();  //リスト宣言 8.3で不要
+        Page<House> housePage = houseRepository.findAll(pageable);  //ページ宣言
+
+        // model.addAttribute("houses",houses);　//8.3で不要
+        model.addAttribute("housePage",housePage);
+
+        return  "admin/houses/index";
+    }
+}

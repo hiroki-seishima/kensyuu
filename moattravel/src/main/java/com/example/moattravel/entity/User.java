ede348bb8ed91d4ba13de228f617ac40923e5f84
hiroki-seishima <yuguiqingdao@gmail.com>
2026-01-13 19:03:04 +0900

    本日の学習

--- moattravel/src/main/java/com/example/moattravel/entity/User.java
+++ moattravel/src/main/java/com/example/moattravel/entity/User.java
@@ -25,6 +25,33 @@ public class User {
     private String name;
 
     @Column(name = "furigana")
-}
+    private String furigana;
 
-// 続きから
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Timestamp updatedAt;
+}
